ea84f36aa42fe8e7ee481c445d2f80c25eb67b4c
Caleb Johnson <cjohn734@students.kennesaw.edu>
2024-07-22 16:18:44 -0400

    Database installed / imported data from excel

--- gamestat_main.py
+++ gamestat_main.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///games.db'
+
+db = SQLAlchemy(app)
+
+class Games(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<Game %r>' % self.id
+    
+def import_excel_data(file_path):
+    data = pd.read_excel(file_path)
+    for _, row in data.iterrows():
+        game = Games(name=row['title'])  
+        db.session.add(game)
+    db.session.commit()
 
 @app.route('/')
 def index():
@@ -9,7 +27,10 @@ def index():
 
 @app.route('/Games')
 def games():
-    return render_template('Games.html')
+    page = request.args.get('page', 1, type=int)
+    per_page = 10
+    games_list = Games.query.paginate(page = page, per_page = per_page, error_out = False)
+    return render_template('Games.html', games=games_list)
 
 @app.route('/action')
 def action():
@@ -36,5 +57,9 @@ def about():
     return render_template('About.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        import_excel_data('c:/Users/leben/OneDrive/Documents/GitHub/Game-Sales-Data-Analysis/vgchartz-2024.xlsx')
+        print("Database tables created.")
     app.run(debug = True)
 
---
+++ init_db.py
@@ -0,0 +1,5 @@
+from gamestat_main import app, db
+
+with app.app_context():
+    db.create_all()
+    print("Database tables created.")
